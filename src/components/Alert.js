507dcd4d6a0f3cd05a213911ae55e8738c6eed77
ScottW3 <77562115+sw-3@users.noreply.github.com>
2023-05-04 21:07:14 -0500

    Adds deposit form

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -5,7 +5,7 @@ const Alert = ({ message, transactionHash, variant, setShowAlert }) => {
     <BootstrapAlert
       variant={variant}
       onClose={() => setShowAlert(false)}
-      dismissable
+      dismissible
       className='alert'
     >
       <BootstrapAlert.Heading>{message}</BootstrapAlert.Heading>
